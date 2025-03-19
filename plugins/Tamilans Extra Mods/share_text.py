04090f6eaa6aafa3ee95ecff1d6d6b81a8c0411f
ak4422920 <ak4422920@gmail.com>
2025-03-19 09:44:05 +0530

    Update share_text.py

--- plugins/Tamilans Extra Mods/share_text.py
+++ plugins/Tamilans Extra Mods/share_text.py
@@ -16,7 +16,7 @@ async def share_text(client, message):
         await message.reply_text(
             text=f"**Nᴏᴛɪᴄᴇ:**\n\n1. Rᴇᴩʟʏ Tᴏ Aɴʏ Mᴇssᴀɢᴇ.\n2. Nᴏ Mᴇᴅɪᴀ Sᴜᴩᴩᴏʀᴛ ﹝ sᴜᴩᴩᴏʀᴛs ᴏɴʟʏ ᴛᴇxᴛ ﹞\n\n**Jᴏɪɴ Nᴏᴡ Oᴜʀ Uᴩᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🙌 Uᴩᴅᴀᴛᴇs 💥", url=f"https://t.me/Tamilan_BotsZ")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🙌 Uᴩᴅᴀᴛᴇs 💥", url=f"https://t.me/AKMovieBotz")]])
             )                                                   
         return
     await message.reply_text(
